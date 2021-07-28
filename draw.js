7ac67a221301b961fd9b019bebe329cadcabc62a
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2021-07-28 14:11:09 +0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,18 +1,26 @@
 const canvas = document.querySelector('canvas');
 const scale = 10;
 var ctx = canvas.getContext('2d');
+
 var snake;
 
-setup();
-function setup() {
+(function setup() {
   snake = new Snake();
+  food = new Food();
+
+  food.pickRandomLocation();
 
   window.setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snake.update();
     snake.draw();
+    food.draw();
+
+    if (snake.eat(food)) {
+      food.pickRandomLocation();
+    }
   }, 125);
-}
+}());
 
 window.addEventListener('keydown', ((event) => {
   var direction = event.key.replace('Arrow', '');
